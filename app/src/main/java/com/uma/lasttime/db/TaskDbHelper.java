3fdbbd10717c275faab3195fa9e4f2a2425101c2
José Manuel <jmsanchezrico@gmail.com>
2023-12-19 23:36:08 +0100

    Added foreign keys on for SQLite initialization

--- app/src/main/java/com/uma/lasttime/db/TaskDbHelper.java
+++ app/src/main/java/com/uma/lasttime/db/TaskDbHelper.java
@@ -5,23 +5,22 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class TaskDbHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 2;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATABASE_NAME = "Task.db";
 
     private static final String SQL_CREATE_TASK_ENTRIES =
             "CREATE TABLE " + TaskContract.TaskEntry.TABLE_NAME + " (" +
                     TaskContract.TaskEntry._ID + " INTEGER PRIMARY KEY," +
                     TaskContract.TaskEntry.COLUMN_NAME_TITLE + " TEXT," +
-                    TaskContract.TaskEntry.COLUMN_NAME_DESCRIPTION + " TEXT)";
+                    TaskContract.TaskEntry.COLUMN_NAME_DESCRIPTION + " TEXT);";
 
     private static final String SQL_CREATE_TIMESTAMP_ENTRIES =
             "CREATE TABLE " + TaskContract.TimestampEntry.TABLE_NAME + " (" +
                     TaskContract.TimestampEntry._ID + " INTEGER PRIMARY KEY," +
                     TaskContract.TimestampEntry.COLUMN_NAME_TIMESTAMP + " DATE," +
-                    TaskContract.TimestampEntry.COLUMN_NAME_TASK_ID + " INTEGER," +
-                    "FOREIGN KEY(" + TaskContract.TimestampEntry.COLUMN_NAME_TASK_ID + ") REFERENCES " +
+                    TaskContract.TimestampEntry.COLUMN_NAME_TASK_ID + " INTEGER REFERENCES " +
                     TaskContract.TaskEntry.TABLE_NAME + "(" + TaskContract.TaskEntry._ID + ") " +
-                    "ON DELETE CASCADE )";
+                    "ON DELETE CASCADE );";
 
     private static final String SQL_DELETE_TASK_ENTRIES =
             "DROP TABLE IF EXISTS " + TaskContract.TaskEntry.TABLE_NAME;
@@ -32,6 +31,13 @@ public class TaskDbHelper extends SQLiteOpenHelper {
     public TaskDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+    @Override
+    public void onOpen(SQLiteDatabase db){
+        super.onOpen(db);
+        db.execSQL("PRAGMA foreign_keys=ON");
+    }
+
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_CREATE_TASK_ENTRIES);
         db.execSQL(SQL_CREATE_TIMESTAMP_ENTRIES);
