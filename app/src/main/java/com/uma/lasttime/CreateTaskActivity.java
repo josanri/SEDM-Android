d52b5ba820d7bbf7a5ab45743c21fc9277dcce12
José Manuel <jmsanchezrico@gmail.com>
2023-12-19 09:42:58 +0100

    Create task activity finished

--- app/src/main/java/com/uma/lasttime/CreateTaskActivity.java
+++ app/src/main/java/com/uma/lasttime/CreateTaskActivity.java
@@ -1,26 +1,54 @@
 package com.uma.lasttime;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateTaskActivity extends AppCompatActivity {
+import com.uma.lasttime.db.TaskContract;
+import com.uma.lasttime.db.TaskDbHelper;
 
+public class CreateTaskActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+    private EditText titleEdit;
+    private EditText descriptionEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_task);
+        titleEdit = findViewById(R.id.editTextTitleCreate);
+        descriptionEdit = findViewById(R.id.editTextDescriptionCreate);
+        findViewById(R.id.textTitleCreate);
+        TaskDbHelper dbHelper = new TaskDbHelper(getApplicationContext());
+        db = dbHelper.getWritableDatabase();
     }
 
     public void addNewTask(View v) {
         Toast toast;
-        if (true) {
-            toast = Toast.makeText(getApplicationContext(), R.string.msg_renew, Toast.LENGTH_LONG);
-        } else {
-            toast = Toast.makeText(getApplicationContext(), R.string.msg_error_on_renew, Toast.LENGTH_LONG);
+        boolean error;
+        ContentValues values;
+        String title, description;
+
+        error = false;
+        title = titleEdit.getText().toString().trim();
+        description = descriptionEdit.getText().toString().trim();
+        values = new ContentValues();
+        values.put(TaskContract.TaskEntry.COLUMN_NAME_TITLE, title);
+        values.put(TaskContract.TaskEntry.COLUMN_NAME_DESCRIPTION, description);
+        try {
+            db.insert(TaskContract.TaskEntry.TABLE_NAME, null, values);
+        } catch (Exception e) {
+            error = true;
         }
-        toast.show();
+        if (error) {
+            Toast.makeText(getApplicationContext(), R.string.msg_error_on_renew, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(getApplicationContext(), R.string.msg_renew, Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
